659b829b6049febe5d8226f43f2387e099ce9849
Michael-Law <mlawsan@outlook.com>
2020-10-22 14:05:06 +0400

    Modifications to Navbar

--- backend/networker/views.py
+++ backend/networker/views.py
@@ -8,7 +8,8 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+
+    queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -17,6 +18,7 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
+
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
