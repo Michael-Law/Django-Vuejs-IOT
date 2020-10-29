fc2388dd95d3437ccd91fc772699d0f71a0fc639
Michael-Law <mlawsan@outlook.com>
2020-10-29 16:15:30 +0400

    Modifications

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="home">
     <navbar />
+
+    <b-card
+      overlay
+      img-src="https://i.postimg.cc/jSpb4VjZ/chuttersnap-f-N603qc-EA7g-unsplash.jpg"
+      img-alt="Card Image"
+      text-variant="white"
+      img-height="600"
+      img-width="150"
+    >
+      <b-card-text>
+        Powered by artificial intelligence and our mesh of internet of things
+        devices, our software will simplify your scheduling, logistics planning
+        and reduce drastically your costs, thus using our sophisticated
+        intelligent nework, you can easily plan your course of action ahead.
+      </b-card-text>
+    </b-card>
     <b-card no-body class="overflow-hidden" style="max-width: 100%">
       <b-row no-gutters>
         <b-col md="6">
@@ -8,12 +24,14 @@
             src="https://i.postimg.cc/jSpb4VjZ/chuttersnap-f-N603qc-EA7g-unsplash.jpg"
             alt="Blabla"
             class="rounded-0"
+            img-height="600"
+            img-width="150"
           ></b-card-img>
         </b-col>
         <b-col md="6">
           <b-card-body title="Your logistics definitely Smarter">
             <p>
-              Powered by artficial intelligence and our mesh of internet of
+              Powered by artificial intelligence and our mesh of internet of
               things devices, our software will simplify your scheduling,
               logistics planning and reduce drastically your costs, thus using
               our sophisticated intelligent nework, you can easily plan your
@@ -23,7 +41,7 @@
         </b-col>
       </b-row>
     </b-card>
-    <h2>Logistics cases addressed</h2>
+    <h2>Logistics cases addressed; Garbage trucks retrieval and routing</h2>
     <p>
       With the continuing growth of our population and the increasing
       consumption of single items articles. Garbage retrieval is becoming a
