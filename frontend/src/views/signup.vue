444d80f009994c065d7b6a363b457589795fc754
Michael-Law <mlawsan@outlook.com>
2020-11-05 16:26:29 +0400

    Sales

--- frontend/src/views/signup.vue
+++ frontend/src/views/signup.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="signup">
     <navbar />
-    <v-card>
-      <v-card-title>Offers for enterprise</v-card-title>
-      <v-slide-group v-model="model" class="pa-4" center-active show-arrows>
-        <register />
-      </v-slide-group>
+    <v-card style="max-width: 50rem" class="mb-2">
+      <v-card-title>Logistics Solutions for entreprise</v-card-title>
+      <b-card-group deck>
+        <v-slide-group>
+          <b-card title="Tier I">
+            <b-card-text>
+              This tier your company will benefit the simple rerouting of the
+              your vehicles based on public data.
+            </b-card-text>
+            <template #footer>
+              <b-button variant="primary">Contact Sales</b-button>
+            </template>
+          </b-card>
+
+          <b-card title="Tier II">
+            <b-card-text>
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </b-card-text>
+            <template #footer>
+              <b-button variant="primary">Contact Sales</b-button>
+            </template>
+          </b-card>
+
+          <b-card title="Tier III">
+            <b-card-text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </b-card-text>
+            <template #footer>
+              <b-button variant="primary">Contact Sales</b-button>
+            </template>
+          </b-card>
+        </v-slide-group>
+      </b-card-group>
     </v-card>
   </div>
 </template>
