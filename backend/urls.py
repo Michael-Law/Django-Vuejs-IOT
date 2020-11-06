cefb788c222afbcad8e8d56ac71b047a24ded0d3
Michael-Law <mlawsan@outlook.com>
2020-11-06 14:34:00 +0400

    Serialisation

--- backend/urls.py
+++ backend/urls.py
@@ -20,12 +20,13 @@ from backend.networker import views
 
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
+    path("", include("snippets.urls")),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
