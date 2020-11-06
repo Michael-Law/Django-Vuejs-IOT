9a09b4e04c905f9723a6f852d7780398a04def9f
Michael-Law <mlawsan@outlook.com>
2020-11-06 09:55:47 +0400

    DjangoRest

--- frontend/src/views/signup.vue
+++ frontend/src/views/signup.vue
@@ -1,41 +1,41 @@
 <template>
   <div class="signup">
     <navbar />
-    <v-card style="max-width: 50rem" class="mb-2">
+    <v-card style="max-width: 100rem" class="mb-2">
       <v-card-title>Logistics Solutions for entreprise</v-card-title>
       <b-card-group deck>
-        <v-slide-group>
-          <b-card title="Tier I">
-            <b-card-text>
-              This tier your company will benefit the simple rerouting of the
-              your vehicles based on public data.
-            </b-card-text>
-            <template #footer>
-              <b-button variant="primary">Contact Sales</b-button>
-            </template>
-          </b-card>
+        <b-card title="Tier I">
+          <b-card-text>
+            This tier your company will benefit the simple rerouting of the your
+            vehicles based on public data.
+          </b-card-text>
+          <template #footer>
+            <b-button variant="primary">Contact Sales</b-button>
+          </template>
+        </b-card>
 
-          <b-card title="Tier II">
-            <b-card-text>
-              This card has supporting text below as a natural lead-in to
-              additional content.
-            </b-card-text>
-            <template #footer>
-              <b-button variant="primary">Contact Sales</b-button>
-            </template>
-          </b-card>
+        <b-card title="Tier II">
+          <b-card-text>
+            Our technician will install custom made IOT devices specifically
+            designed for your company needs, which will aggregate data for your
+            specific needs. Then doing an non-generic optimisation.
+          </b-card-text>
+          <template #footer>
+            <b-button variant="primary">Contact Sales</b-button>
+          </template>
+        </b-card>
 
-          <b-card title="Tier III">
-            <b-card-text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </b-card-text>
-            <template #footer>
-              <b-button variant="primary">Contact Sales</b-button>
-            </template>
-          </b-card>
-        </v-slide-group>
+        <b-card title="Tier III">
+          <b-card-text>
+            Using blockchain verify your transactions and tag along your
+            different data points by adding to our blockchain infracstructure.
+            Thus you could be sure there will be no amering of data and seamless
+            automatic transactions with your stakeholders
+          </b-card-text>
+          <template #footer>
+            <b-button variant="primary">Contact Sales</b-button>
+          </template>
+        </b-card>
       </b-card-group>
     </v-card>
   </div>
