c4c364c0e6fd016006f3b45c6926dfab2fc1c653
Michael-Law <mlawsan@outlook.com>
2020-11-22 17:40:31 +0400

    Json Web tokens

--- backend/urls.py
+++ backend/urls.py
@@ -16,17 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from backend.networker import views
-
-
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include(router.urls)),
     path("", include("snippets.urls")),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
